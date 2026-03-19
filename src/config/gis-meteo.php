03f62b228db92f4d4deccaf169645d4666103317
Ermak <los-van@mail.ru>
2026-03-19 08:30:15 +0500

    feat: Update ttl gis-meteo.php

--- src/config/gis-meteo.php
+++ src/config/gis-meteo.php
@@ -4,7 +4,7 @@ return [
     'auth' => [
         'token' => env('GIS_METEO_AUTH_TOKEN', ''),
     ],
-    'cache_ttl' => env('GIS_METEO_CACHE_TTL', 86400), //3600*24
+    'cache_ttl' => env('GIS_METEO_CACHE_TTL', 3600), //3600*24
     'default_city' => env('GIS_METEO_CITY', 'Moscow'),
     'debug_data' => '{}'
 ];
